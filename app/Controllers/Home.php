e6b8fd256ccf64c93277d168eb629208afa53b52
haridarmawann <hdarmawan472@gmail.com>
2021-03-31 01:56:19 +0700

    menambahkan fungsi update dan delete

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -45,4 +45,34 @@ class Home extends BaseController
 		
 		return redirect()->to('/home');
 	}
+	public function edit()
+	{
+		$data = [
+			'title' =>'form pendaftaran killjoy Bootcamp'
+		];
+
+		return view('killjoy/update');
+	}
+
+	public function update($id){
+		$this->homeModel->save([
+			'id' => $id,
+			'nama' => $this->request->getVar('nama'),
+			'email' => $this->request->getVar('email'),
+			'jeniskelamin' => $this->request->getVar('jeniskelamin'),
+			'agama' => $this->request->getVar('agama'),
+			'alamat' => $this->request->getVar('alamat'),
+			'nick' => $this->request->getVar('nick'),
+			'hastag' => $this->request->getVar('hastag'),
+			'hero' => $this->request->getVar('hero'),
+			'server' => $this->request->getVar('server'),
+		]);
+
+		session()->setFlashdata('pesan','Data berhasil diubah');
+	}
+
+	public function delete($id){
+		$this->homeModel->delete($id);
+		return redirect()->to('/home');
+	}
 }
